5b30fa32f789a87a0a5ca52170f1e52e3c9d3424
Hugo Peral <hugoj.peral@gmail.com>
2020-11-19 09:42:16 +0100

    Added initial structure of the project

---
+++ Trackify/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Trackify
+//
+//  Created by Hugo Peral on 19/11/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
