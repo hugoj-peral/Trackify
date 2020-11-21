88be49db5506c7cb544ac436efc1d49aed1e57c3
Hugo Peral <hugoj.peral@gmail.com>
2020-11-21 12:14:03 +0100

    Added all the logic to add transaction presenter

--- Trackify/Modules/AddTransaction/Presenter/AddTransactionPresenter.swift
+++ Trackify/Modules/AddTransaction/Presenter/AddTransactionPresenter.swift
@@ -8,11 +8,34 @@
 import Foundation
 
 final class AddTransactionPresenter {
+    
     weak var view: AddTransactionViewProtocol?
     let router: AddTransactionRouterProtocol
     
+    var categoryTypeSelected: CategoryType = .income
+    var accountSelected: Account?
+    var categorySelected: Category?
+    var amountSelected: Double?
+    
+    var listOfCategories = DashboardRandomGenerator().makeAllCategories()
+    var listOfAccounts =  DashboardRandomGenerator().makeAllAccounts(transactionDistribution: [:])
+    
+    // MARK : List variables
+    enum ListOption {
+        case account, category
+    }
+    var listSelected: ListOption!
+    var listDataSource: [String]!
+    
     init(router: AddTransactionRouterProtocol) {
         self.router = router
+    }
+    
+    func validateData() {
+        guard let amount = amountSelected,
+              let category = categorySelected,
+              let account = accountSelected else { return }
+        router.doneDismiss()
     }
 }
 
@@ -31,34 +54,55 @@ extension AddTransactionPresenter: AddTransactionPresenterProtocol {
     }
     
     func doneAction() {
-        router.doneDismiss()
+        validateData()
     }
     
     func categoryTypeSelected(type: CategoryType) {
-        
+        categoryTypeSelected = type
+        view?.setSelectedCategory("")
+        categorySelected = nil
     }
     
     func selectAccountAction() {
-        
+        listSelected = .account
+        listDataSource = listOfAccounts.map { $0.name }
+        if accountSelected == nil {
+            accountSelected = listOfAccounts.first
+            view?.setSelectedAccount(accountSelected!.name)
+        }
     }
     
     func selectCategoryAction() {
-        
+        listSelected = .category
+        listDataSource = listOfCategories.filter({$0.type == categoryTypeSelected}).map { $0.name }
+        if categorySelected == nil {
+            categorySelected = listOfCategories.filter({$0.type == categoryTypeSelected}).first
+            view?.setSelectedCategory(categorySelected!.name)
+        }
     }
     
     func amountSelected(amount: String) {
-        
+        amountSelected = Double(amount)
     }
     
     func numberOfItemsToPick() -> Int {
-        return 3
+        return listDataSource.count
     }
     
     func pickerItem(row: Int) -> String {
-        return "Hello"
+        return listDataSource[row]
     }
     
     func pickerItemSelected(row: Int) {
-        
+        switch listSelected {
+        case .account:
+            accountSelected = listOfAccounts[row]
+            view?.setSelectedAccount(accountSelected!.name)
+        case .category:
+            categorySelected = listOfCategories[row]
+            view?.setSelectedCategory(categorySelected!.name)
+        default:
+            break
+        }
     }
 }
